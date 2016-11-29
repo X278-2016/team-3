870e2e79d5b7d400b8bb8a26ea4ec7a23500f11c
Connor Goggans <connor.goggans@vanderbilt.edu>
2016-11-29 12:10:09 -0600

    Added Comments field to Patient and fixed Patient edit dialog box

--- existing_code/manatee/src/main/webapp/app/entities/patient/patient.state.js
+++ existing_code/manatee/src/main/webapp/app/entities/patient/patient.state.js
@@ -68,6 +68,7 @@
                                 conditionDesciption: null,
                                 priority: null,
                                 deadline: null,
+                                comments: null,
                                 id: null
                             };
                         }
@@ -104,34 +105,6 @@
                 });
             }]
         })
-        //History Controller
-        /*
-        .state('patient.history', {
-            parent: 'patient',
-            url: '/{id}/history',
-            data: {
-                authorities: ['ROLE_USER']
-            },
-            onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
-                $uibModal.open({
-                    templateUrl: 'app/entities/patient/patient-dialog.html',
-                    controller: 'PatientDialogController',
-                    controllerAs: 'vm',
-                    backdrop: 'static',
-                    size: 'lg',
-                    resolve: {
-                        entity: ['Patient', function(Patient) {
-                            return Patient.get({id : $stateParams.id});
-                        }]
-                    }
-                }).result.then(function() {
-                    $state.go('patient', null, { reload: true });
-                }, function() {
-                    $state.go('^');
-                });
-            }]
-        })
-        */
         .state('patient.delete', {
             parent: 'patient',
             url: '/{id}/delete',
