1b24f5f56c33ecef992b77a7423954c2187691ba
Fangzhou Sun <fzsun316@gmail.com>
2016-12-11 18:36:32 -0600

    Fixed a bug

--- existing_code/manatee/src/main/webapp/app/entities/queue/queue.controller.js
+++ existing_code/manatee/src/main/webapp/app/entities/queue/queue.controller.js
@@ -139,27 +139,29 @@
                         standardItems.push({row: Math.floor(i_team/3)*3, col: (i_team%3)*2});
                     }
                     $scope.standardItems = standardItems;
+
                     $( ".connectedSortable" ).sortable({
-      connectWith: ".connectedSortable",
-      items: "tr",
-      opacity: 0.5,
-      revert: 400,
-      receive: function(event, ui) {
-        var id = $(ui.item).attr('id');
-        var teamID = this.id;
-        if (id=="potentialdischarge-tr" || id==-1) {
-          ui.sender.sortable("cancel");
-          return;
-        }
-        if (teamID=="potentialdischarge") {
-          ui.sender.sortable("cancel");
-          $('#QueueController').scope().updateStatus(id, "potentialdischarge");
-        } else {
-          $('#QueueController').scope().updateTeam(id, teamID);
-        }
-        // console.log(id +"  receive: "+ teamID);
-      },
-    }).disableSelection();
+                      connectWith: ".connectedSortable",
+                      items: "tr",
+                      opacity: 0.5,
+                      revert: 400,
+                      receive: function(event, ui) {
+                        console.log("fadsfasdfsad");
+                        var id = $(ui.item).attr('id');
+                        var teamID = this.id;
+                        if (id=="potentialdischarge-tr" || id==-1) {
+                          ui.sender.sortable("cancel");
+                          return;
+                        }
+                        if (teamID=="potentialdischarge") {
+                          ui.sender.sortable("cancel");
+                          $('#QueueController').scope().updateStatus(id, "potentialdischarge");
+                        } else {
+                          $('#QueueController').scope().updateTeam(id, teamID);
+                        }
+                        // console.log(id +"  receive: "+ teamID);
+                      },
+                    }).disableSelection();
                     $scope.arrayPatientTeam = arrayPatientTeam;
                     $scope.arrayPotentialDischargedPatient = arrayPotentialDischargedPatient;
                     $scope.arrayIncomingPatient = arrayIncomingPatient;
